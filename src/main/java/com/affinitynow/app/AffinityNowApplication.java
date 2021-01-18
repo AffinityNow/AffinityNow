7c7bc7e2ee45a02afa8eb6f7a1b217d4331a7b20
AffinityNow <72141642+AffinityNow@users.noreply.github.com>
2021-01-18 17:45:05 +0100

    Add files via upload

---
+++ src/main/java/com/affinitynow/app/AffinityNowApplication.java
@@ -0,0 +1,13 @@
+package com.affinitynow.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffinityNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AffinityNowApplication.class, args);
+	}
+
+}
